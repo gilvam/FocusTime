ae4ab83ddef37ca7e04e6ceee8c5ffcbd91a7c11
Gilvam Mourao <gilvamm@gmail.com>
2024-01-05 17:24:51 -0300

    chore: init

--- App.tsx
+++ App.tsx
@@ -1,20 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { Platform, SafeAreaView, StatusBar, StyleSheet, Text } from 'react-native';
+import { colors } from './src/utils/colors';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<SafeAreaView style={styles.container}>
+			<Text style={styles.text}>Hello World</Text>
+		</SafeAreaView>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+		backgroundColor: colors.darkBlue
+	},
+	text: {
+		color: colors.white
+	}
 });
---
+++ README.md
@@ -0,0 +1,3 @@
+# FocusTime
+
+### a reactive native app

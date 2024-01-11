4edd36086aa0afc944d947321fd199efde500829
Gilvam Mourao <gilvamm@gmail.com>
2024-01-11 16:38:32 -0300

    feat: adding text input

--- App.tsx
+++ App.tsx
@@ -1,12 +1,14 @@
 import * as React from 'react';
-import { Platform, SafeAreaView, StatusBar, StyleSheet } from 'react-native';
+import { Platform, SafeAreaView, StatusBar, StyleSheet, View } from 'react-native';
+import { TextInput } from 'react-native-paper';
 import { colors } from './src/utils/colors';
-import { Focus } from './src/utils/Focus';
 
 export default function App() {
 	return (
 		<SafeAreaView style={styles.container}>
-			<Focus />
+			<View style={styles.inputContainer}>
+				<TextInput label="What would you like to focus on?" />
+			</View>
 		</SafeAreaView>
 	);
 }
@@ -16,6 +18,12 @@ const styles = StyleSheet.create({
 		flex: 1,
 		paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
 		backgroundColor: colors.darkBlue,
+	},
+	inputContainer: {
+		flex: 0.5,
+		padding: 25,
+		justifyContent: 'flex-start',
+		backgroundColor: '#aaa',
 	},
 	text: {
 		color: colors.white,
